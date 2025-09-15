40785c69c310a9ced168bb4897d5dff71890ed06
Colin Birdwell <Colin.birdwell@paymentdata.com>
2025-09-15 15:31:54 -0400

    added h1

--- src/components/molecules/Logo/index.tsx
+++ src/components/molecules/Logo/index.tsx
@@ -8,19 +8,30 @@ import { ENV_TITLE, URL_HOME } from "@/constants";
 export const Logo: React.FC<LogoProps> = ({ onDismiss }) => {
   const navigate = useNavigate();
   const navigateHandler = () => {
+    onDismiss();
+
     navigate(URL_HOME);
   };
 
   return (
-    <div className="logo" onClick={onDismiss}>
-      <ReactSVG
-        onClick={navigateHandler}
-        src={logo}
-        className="logo_svg"
-        title={`Logo for ${ENV_TITLE}.`}
-        wrapper="svg"
-      />
-      <div className="backdrop" />
+    <div className="logo" onClick={navigateHandler}>
+      <div className="logo_image">
+        <ReactSVG
+          src={logo}
+          className="logo_image_svg"
+          title={`Logo for ${ENV_TITLE}.`}
+          wrapper="svg"
+        />
+        <div className="logo_image_backdrop" />
+      </div>
+
+      <h1>
+        <span className="top">
+          <span className="top_left">Web</span>
+          <span className="top_right"> By</span>
+        </span>
+        <span className="bottom">Birdwell</span>
+      </h1>
     </div>
   );
 };
