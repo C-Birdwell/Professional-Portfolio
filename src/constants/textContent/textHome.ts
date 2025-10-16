8b2688b11c6f3985ea8a7db89a8247618e79f622
Colin Birdwell <Colin.birdwell@paymentdata.com>
2025-10-16 15:22:42 -0400

    switched const variables textHome_5 and _4

--- src/constants/textContent/textHome.ts
+++ src/constants/textContent/textHome.ts
@@ -9,9 +9,9 @@ export const textHome_2 = `Along with his considerable experience in the private
 
 export const textHome_3 = `${ENV_OWNER} excels in the React library leveraging Javascript, Typescript, Redux, SaSS, CSS, and HTML to create dynamic and interactive user interfaces.`;
 
-export const textHome_5 = `With his expertise in design and development ${ENV_OWNER} is able to shape and guide ideation to shippable products.`;
+export const textHome_4 = `With his expertise in design and development ${ENV_OWNER} is able to shape and guide ideation to shippable products.`;
 
-export const textHome_4 = `Along with his expertise with frontend development ${ENV_OWNER} also has skills in the backend with Python and Flask. ${ENV_OWNER} also holds multiple AWS certifications.`;
+export const textHome_5 = `Along with his expertise with frontend development ${ENV_OWNER} also has skills in the backend with Python and Flask. ${ENV_OWNER} also holds multiple AWS certifications.`;
 
 export const textHomeData = [
   textHome_0,
